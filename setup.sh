db2adb4a0d13e0cc1fce2766ea9d4e8fb71a84fe
jaehokang <jhkang@illunex.com>
2023-01-03 14:15:21 +0900

    python project root 수정

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!bin/bash
-python3 -m virtualenv | grep -q 'No module named' && pip3 install virtualenv || echo 'Virtualenv is installed'
-virtualenv venv
-source venv/bin/activate
-python3 -m pre-commit | grep -q 'No module named' && pip3 install pre-commit || echo 'pre-commit is installed'
-python3 -m pytest | grep -q 'No module named' && pip3 install pytest || echo 'pre-commit is installed'
-pip3 install -r requirements.txt
+# python3 -m virtualenv | grep -q 'No module named' && pip3 install virtualenv || echo 'Virtualenv is installed'
+# virtualenv venv
+# source venv/bin/activate
+# python3 -m pre-commit | grep -q 'No module named' && pip3 install pre-commit || echo 'pre-commit is installed'
+# python3 -m pytest | grep -q 'No module named' && pip3 install pytest || echo 'pre-commit is installed'
+# pip3 install -r requirements.txt
 mv setup-folder/pre-push .git/hooks/pre-push
 chmod +x .git/hooks/pre-push
 rm README.md
